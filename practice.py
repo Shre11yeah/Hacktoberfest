2027c2c5adbdaefcf4eb73fd72962b53c3f59be1
Shreya Bangale <130304646+Shre11yeah@users.noreply.github.com>
2023-10-02 17:51:57 +0530

    Create practice.py

---
+++ practice.py
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+ 
+// function declaration:
+double getAverage(int arr[], int size);
+
+int main () {
+   // an int array with 5 elements.
+   int balance[5] = {1000, 2, 3, 17, 50};
+   double avg;
+
+   // pass pointer to the array as an argument.
+   avg = getAverage( balance, 5 ) ;
+ 
+   // output the returned value 
+   cout << "Average value is: " << avg << endl; 
+    
+   return 0;
+}
